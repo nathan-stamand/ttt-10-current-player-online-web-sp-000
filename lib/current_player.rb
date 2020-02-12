ffb88c8fddd55b8c3bbd4a71c07e952bb730386f
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-12 17:25:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,11 @@ def turn_count(board)
   new_board = []
   board.each do |character|
     if character == "X"
-      
+      new_board << character
+    elsif character == "O"
+      new_board << character
+    end
+  return new_board.count; 
 end
 
 def current_player(board)
